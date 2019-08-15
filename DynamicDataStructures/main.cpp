fe97a9269a293d06a3bd63fa81d0f2e2d7aff180
Student <Student@201-11>
2019-08-15 19:00:33 +0300

    ForwardList

---
+++ DynamicDataStructures/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Element
+{
+	int Data;
+	Element* pNext;
+public:
+	Element(int Data, Element* pNext = nullptr)
+	{
+		this->Data = Data;
+		this->pNext = pNext;
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+};
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+}
